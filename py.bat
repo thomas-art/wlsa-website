5ef491daeebca156ee57f5f1e2e7c5112f14d308
LanYangYang321 <1492141580@qq.com>
2024-10-01 13:02:42 +0800

    still tiny changes in bat files

--- py.bat
+++ py.bat
@@ -1,24 +1,25 @@
 @echo off
 chcp 65001
-echo successfully set cmd to utf-8.
+echo py.bat: successfully set cmd to utf-8.
 set currentDir=%~dp0
 cd /d "%currentDir%\webpy2"
-echo successfully cd to currendir.
+echo py.bat: successfully cd to currendir.
+echo py.bat: current py dir: %cd%
 
-echo check if conda exist...
+echo py.bat: check if conda exist...
 where conda > nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo cannot find conda, use normal python.
+    echo py.bat: cannot find conda, use normal python.
     python app.py %1
 ) ELSE (
-    echo checking conda envs...
+    echo py.bat: checking conda envs...
     conda env list | findstr /C:"pyweb"
     IF %ERRORLEVEL%==0 (
-        echo found conda env, use conda env.
+        echo py.bat: found conda env, use conda env.
         call conda activate pyweb
         python app.py %1
     ) ELSE (
-        echo cannot find conda env, use normal python.
+        echo py.bat: cannot find conda env, use normal python.
         python app.py %1
     )
 )
